cccfe6faf4d5b3f29a93f9d788559d2026df7a90
Shreyash <shreyashanindya07@gmail.com>
2025-02-25 19:13:07 +0530

    added course card and course section in homepage

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,11 +1,24 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { dummyCourses } from "../assets/assets";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props)=>{
 
-    const value = {
+    const currency = import.meta.env.VITE_CURRENCY
+    const [allCourses, setAllCourses] = useState([])
 
+    //Fetch all courses
+    const fetchAllCourses = async ()=>{
+        setAllCourses(dummyCourses)
+    }
+
+    useEffect(()=>{
+        fetchAllCourses()
+    },[])
+
+    const value = {
+        currency, allCourses
     }
 
     return (
