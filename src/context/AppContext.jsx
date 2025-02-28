5f18887cf6dc1750c203b74f863d58e1186352f1
Shreyash <shreyashanindya07@gmail.com>
2025-02-28 19:40:44 +0530

    added overview and course structure in course details page

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,13 +1,14 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+import humanizeDuration from "humanize-duration"
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props)=>{
 
     const currency = import.meta.env.VITE_CURRENCY
-    const navigate=  useNavigate()
+    const navigate= useNavigate()
 
     const [allCourses, setAllCourses] = useState([])
     const [isEducator, setIsEducator] = useState(true)
@@ -28,12 +29,40 @@ export const AppContextProvider = (props)=>{
         return totalRating/course.courseRatings.length;
     }
 
+    //Function to calculate course chapter time
+    const calculateChapterTime = (chapter)=>{
+        let time = 0
+        chapter.chapterContent.map((lecture)=> time+= lecture.lectureDuration)
+        return humanizeDuration(time*60*1000, {units: ["h","m"]})
+    }
+
+    //Function to calculate Course duration
+    const calculateCourseDuration = (course)=>{
+        let time=0
+        course.courseContent.map((chapter)=> chapter.chapterContent.map(
+            (lecture) => time+= lecture.lectureDuration
+        ))
+        return humanizeDuration(time*60*1000, {units: ["h","m"]})
+    }
+
+    //Function to calculate the no. of lectures in the course
+    const calculateNoOfLectures= (course)=>{
+        let totalLectures = 0;
+        course.courseContent.forEach(chapter => {
+            if(Array.isArray(chapter.chapterContent)){
+                totalLectures += chapter.chapterContent.length;
+            }
+        })
+        return totalLectures;
+    }
+
     useEffect(()=>{
         fetchAllCourses()
     },[])
 
     const value = {
-        currency, allCourses, navigate, calculateRating, isEducator, setIsEducator
+        currency, allCourses, navigate, calculateRating, isEducator, setIsEducator,
+        calculateNoOfLectures, calculateCourseDuration, calculateChapterTime
     }
 
     return (
