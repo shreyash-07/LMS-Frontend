2c4a7d22bf52836a7d368f4ca633c376001eebba
Shreyash <shreyashanindya07@gmail.com>
2025-03-01 15:15:00 +0530

    Added preview functionality in the course structure

--- src/pages/student/CourseDetails.jsx
+++ src/pages/student/CourseDetails.jsx
@@ -5,6 +5,8 @@ import Loading from '../../components/student/Loading.jsx';
 import { assets } from '../../assets/assets.js';
 import humanizeDuration from 'humanize-duration';
 import Footer from '../../components/student/Footer.jsx';
+import YouTube from 'react-youtube';
+
 
 const CourseDetails = () => {
 
@@ -12,7 +14,8 @@ const CourseDetails = () => {
 
   const [courseData, setCourseData] = useState(null);
   const [openSections, setOpenSections] = useState({});
-  const [isAlreadyEnrolled, setIsAlreadyEnrolled] = useState(false)
+  const [isAlreadyEnrolled, setIsAlreadyEnrolled] = useState(false);
+  const [playerData, setPlayerData] = useState(null);
 
   const {allCourses, calculateRating, calculateNoOfLectures,
      calculateCourseDuration, calculateChapterTime, currency} = useContext(AppContext);
@@ -95,7 +98,13 @@ const CourseDetails = () => {
                           text-xs md:text-default'>
                             <p>{lecture.lectureTitle}</p>
                             <div className='flex gap-2'>
-                              {lecture.isPreviewFree && <p className='text-blue-500 cursor-pointer'>Preview</p>}
+                              {lecture.isPreviewFree &&
+                               <p className='text-blue-500 cursor-pointer'
+                                onClick={()=>setPlayerData({
+                                  videoId: lecture.lectureUrl.split('/').pop()
+                               })}>
+                                Preview
+                              </p>}
                               <p>{humanizeDuration(lecture.lectureDuration * 60 * 1000, {units: ['h','m']})}</p>
                             </div>
                           </div>
@@ -121,10 +130,19 @@ const CourseDetails = () => {
       {/* right column */}
       <div className='max-w-course-card z-10 shadow-custom-card rounded-t md:rounded-none overflow-hidden bg-white
       min-w-[300px] sm:min-w-[420px]'>
-        <img src={courseData.courseThumbnail} alt="" />
+        
+        {
+              playerData ? 
+              <YouTube videoId={playerData.videoId} opts={{playerVars:{autoplay:1 }}}
+              iframeClassName="w-full aspect-video" />
+              :   <img src={courseData.courseThumbnail} alt="" />
+            }
+       
         <div className='p-5'>
           <div className='flex items-center gap-2'>
-            <img className='w-3.5' src={assets.time_left_clock_icon } alt="time left clock icon" />
+
+          <img className='w-3.5' src={assets.time_left_clock_icon } alt="time left clock icon" />
+
             <p className='text-red-500'><span className='font-medium'>5 days</span> left at this price!</p>
           </div>
 
