d73451a642e327718b0bd22175f8a04c039a3b83
Shreyash <shreyashanindya07@gmail.com>
2025-03-01 14:46:46 +0530

    added the right section in the course details page

--- src/pages/student/CourseDetails.jsx
+++ src/pages/student/CourseDetails.jsx
@@ -4,6 +4,7 @@ import { AppContext } from '../../context/AppContext';
 import Loading from '../../components/student/Loading.jsx';
 import { assets } from '../../assets/assets.js';
 import humanizeDuration from 'humanize-duration';
+import Footer from '../../components/student/Footer.jsx';
 
 const CourseDetails = () => {
 
@@ -11,9 +12,10 @@ const CourseDetails = () => {
 
   const [courseData, setCourseData] = useState(null);
   const [openSections, setOpenSections] = useState({});
+  const [isAlreadyEnrolled, setIsAlreadyEnrolled] = useState(false)
 
   const {allCourses, calculateRating, calculateNoOfLectures,
-     calculateCourseDuration, calculateChapterTime} = useContext(AppContext);
+     calculateCourseDuration, calculateChapterTime, currency} = useContext(AppContext);
 
   const fetchCoursesData = async()=>{
     const findCourse = allCourses.find(course => course._id === id);
@@ -117,11 +119,71 @@ const CourseDetails = () => {
       </div>
 
       {/* right column */}
-      <div>
+      <div className='max-w-course-card z-10 shadow-custom-card rounded-t md:rounded-none overflow-hidden bg-white
+      min-w-[300px] sm:min-w-[420px]'>
+        <img src={courseData.courseThumbnail} alt="" />
+        <div className='p-5'>
+          <div className='flex items-center gap-2'>
+            <img className='w-3.5' src={assets.time_left_clock_icon } alt="time left clock icon" />
+            <p className='text-red-500'><span className='font-medium'>5 days</span> left at this price!</p>
+          </div>
 
+          <div className='flex gap-3 items-center pt-2'>
+          <p className='text-gray-800 md:text-4xl text-2xl font-semibold'>
+            {currency}{(courseData.coursePrice - courseData.discount * courseData.coursePrice /100).toFixed(2)}
+          </p>
+          <p className='md:text-lg text-gray-500 line-through'>
+            {currency}{courseData.coursePrice}
+          </p>
+          <p className='md:text-lg text-gray-500'>
+            {currency}{courseData.discount}% off
+          </p>
+          </div>
+
+          <div className='flex items-center text-sm md:text-default gap-4 pt-2 md:pt-4 text-gray-500'>
+            <div className='flex items-center gap-1 '>
+              <img src={assets.star} alt="star icon" />
+              <p>{calculateRating(courseData)}</p>
+            </div>
+
+            <div className='h-4 w-px bg-gray-500/40'></div>
+
+            <div className='flex items-center gap-1 '>
+              <img src={assets.time_clock_icon} alt="clock icon" />
+              <p>{calculateCourseDuration(courseData)}</p>
+            </div>
+
+            <div className='h-4 w-px bg-gray-500/40'></div>
+
+            <div className='flex items-center gap-1 '>
+              <img src={assets.lesson_icon} alt="clock icon" />
+              <p>{calculateNoOfLectures(courseData)} lessons</p>
+            </div>
+
+          </div>
+
+          <button className='md:mt-6 mt-4 w-full py-3 rounded bg-blue-600 text-white font-medium'>
+            {isAlreadyEnrolled ? "Already Enrolled" : "Enroll Now"}
+          </button>
+          
+          <div className='pt-6'>
+            <p className='md:text-xl text-lg font-medium text-gray-800 '>
+              What's in the course?
+            </p>
+            <ul className='ml-4 pt-2 text-sm md:text-default list-disc text-gray-500'>
+              <li>Lifetime access with free updates.</li>
+              <li>Step-by-step, hands-on project guidance.</li>
+              <li>Downloadable resources and source code.</li>
+              <li>Quizzes to test your knowledge.</li>
+              <li>Certificate of completion.</li>
+            </ul>
+          </div>
+
+        </div>
       </div>
 
     </div>
+    <Footer />
     </>
   ) : <Loading />
 }
